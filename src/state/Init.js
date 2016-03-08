9d187bba47b15162cb2cde0d7e77cecf71037549
rabrennie <rennierab@gmail.com>
2016-03-08 16:23:55 +0000

    fixed model loading

--- src/state/Init.js
+++ src/state/Init.js
@@ -4,7 +4,7 @@ import Simplicity from '../Simplicity';
 class Init extends State {
   preload() {
     var loader = new THREE.JSONLoader();
-    loader.load('../assets/test.json', function(geometry) {
+    loader.load('./assets/test.json', function(geometry) {
       Simplicity.models.spikes = geometry;
       Simplicity.StateManager.load('test');
     });
